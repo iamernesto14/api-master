29db3202a0355e73abb50a16cd8f4ffeaeabaaac
iamernesto14 <eernesto211@gmail.com>
2025-06-18 10:20:18 +0000

    Update route to include single post

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { PostsListComponent } from './components/posts-list/posts-list';
+import { SinglePost } from './components/single-post/single-post';
 
 export const routes: Routes = [
-  { path: '', component: PostsListComponent },
+    { path: '', component: PostsListComponent },
+  {path: 'post/:id', component: SinglePost},
   { path: '**', redirectTo: '' }
 ];
