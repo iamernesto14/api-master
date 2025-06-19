1dba995f2765c3ad8e68d136d3d53cd9d3e259b3
iamernesto14 <eernesto211@gmail.com>
2025-06-19 11:29:41 +0000

    add edit post functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { SinglePostComponent } from './components/single-post/single-post';
 import { LoginComponent } from './components/login/login';
 import { AuthGuard } from './guards/auth-guard';
 import { CreatePostComponent } from './components/create-post/create-post';
+import { EditPostComponent } from './components/edit-post/edit-post';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', component: PostsListComponent, canActivate: [AuthGuard] },
   { path: 'post/:id', component: SinglePostComponent, canActivate: [AuthGuard] },
   { path: 'create-post', component: CreatePostComponent, canActivate: [AuthGuard] },
+  { path: 'edit-post/:id', component: EditPostComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: 'login' }
 ];
